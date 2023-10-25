1b2feb9b3a69c6b46ca3e28b8f4307b2f4f25091
Javad Jamal <javad.jama77@gmail.com>
2023-10-25 21:11:12 +0400

    first commit

---
+++ scripts/php/phpmailer/config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * ======================================
+ * HTWF - SMTP CONFIG FILE
+ * ======================================
+ *
+ * From this file you can set SMTP informations.
+ * To enable the SMTP on your contact form add the attribute data-engine="smtp" to the form object.
+ * Documentation at html.framework-y.com/components/contact-form/
+ */
+
+$smtp_config = array(
+    'host' => 'smtps.aruba.it',
+    'username' => 'federico@pixor.it',
+    'password' => 'fede$k1o',
+    'port' => '465',
+    'email_from' => 'mail@pixor.it'
+);
+?>
